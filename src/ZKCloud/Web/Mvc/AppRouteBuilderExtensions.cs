707057e3c21b76c8b4e654d4f067fa0008665adc
sampsong <sampsong@qq.com>
2016-03-30 22:27:45 +0800

    修改插件的命名方式，以及命名空间

---
+++ src/ZKCloud/Web/Mvc/AppRouteBuilderExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZKCloud.Web.Mvc
+{
+    public static class  AppRouteBuilderExtensions
+    {
+        public static IRouteBuilder MapAppRoute(this IRouteBuilder routeCollectionBuilder, string name, string template)
+        {
+            MapAppRoute(routeCollectionBuilder, name, template, defaults: null);
+            return routeCollectionBuilder;
+        }
+
+        public static IRouteBuilder MapAppRoute(this IRouteBuilder routeCollectionBuilder, string name, string template, object defaults)
+        {
+            return MapAppRoute(routeCollectionBuilder, name, template, defaults, constraints: null, dataTokens: null);
+        }
+
+        public static IRouteBuilder MapAppRoute(this IRouteBuilder routeCollectionBuilder, string name, string template, object defaults, object constraints, object dataTokens)
+        {
+            var inlineConstraintResolver = (IInlineConstraintResolver)routeCollectionBuilder.ServiceProvider.GetService(typeof(IInlineConstraintResolver));
+            routeCollectionBuilder.Routes.Add(
+                new AppTemplateRoute(
+                    routeCollectionBuilder.DefaultHandler,
+                    name,
+                    template,
+                    ObjectToDictionary(defaults),
+                    ObjectToDictionary(constraints),
+                    ObjectToDictionary(dataTokens),
+                    inlineConstraintResolver));
+
+            return routeCollectionBuilder;
+        }
+
+        private static IDictionary<string, object> ObjectToDictionary(object value)
+        {
+            var dictionary = value as IDictionary<string, object>;
+            if (dictionary != null)
+            {
+                return dictionary;
+            }
+
+            return new RouteValueDictionary(value);
+        }
+    }
+}
