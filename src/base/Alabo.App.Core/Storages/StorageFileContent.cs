195a67c421790123ee1a7720242dc434e1363cdf
zkcloud <asd@qq.com>
2019-09-20 16:14:47 +0800

    重构文件系统

--- src/base/Alabo.App.Core/Storages/StorageFileContent.cs
+++
@@ -1,8 +0,0 @@
-﻿using Alabo.App.Core.Common.Domain.Entities;
-
-namespace Alabo.App.Core.Common {
-
-    public class StorageFileContent : StorageFile {
-        public byte[] Body { get; set; }
-    }
-}
