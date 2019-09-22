a83f704d110ad28444c10121ffafe89379ae939f
zkcloud <asd@qq.com>
2019-09-22 12:07:32 +0800

    移动文件夹

--- src/02.data/01-Alabo.Data.People/Users/TenantDefault.cs
+++ src/02.data/01-Alabo.Data.People/Users/TenantDefault.cs
@@ -27,6 +27,18 @@ namespace Alabo.App.Core.User {
         /// init
         /// </summary>
         public void Init() {
+            //导入地址数据 ,从js文件导入,如果从高德地图导入会导致地址库不同步的问题
+            Ioc.Resolve<IRegionService>().InitRegion();
+
+            //// 导入商圈数据
+            //Ioc.Resolve<ICircleService>().Init();
+
+            //// 导入Schedule数据
+            //Ioc.Resolve<IScheduleService>().Init();
+
+            //// 初始化所有没有资产账号的会员
+            //Ioc.Resolve<IAccountService>().InitAllUserIdsWidthOutAccount();
+
             // 自动配置相关数据
             Ioc.Resolve<IAutoConfigService>().InitDefaultData();
 
