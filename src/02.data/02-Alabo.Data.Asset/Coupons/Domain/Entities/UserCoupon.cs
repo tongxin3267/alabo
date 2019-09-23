edff12a75dabc5e399a80644cb1efef48d3edba4
zkcloud <asd@qq.com>
2019-09-23 22:25:20 +0800

    修改数据结构

---
+++ src/02.data/02-Alabo.Data.Asset/Coupons/Domain/Entities/UserCoupon.cs
@@ -0,0 +1,111 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Alabo.App.Shop.Coupons.Domain.Enums;
+using Alabo.App.Shop.Coupons.Domain.Services;
+using Alabo.Domains.Entities;
+using Alabo.Domains.Enums;
+using Alabo.UI;
+using Alabo.UI.AutoTables;
+using Alabo.Web.Mvc.Attributes;
+
+namespace Alabo.App.Shop.Coupons.Domain.Entities {
+
+    /// <summary>
+    /// 用户优惠券
+    /// </summary>
+    [Table("Asset_User")]
+    [BsonIgnoreExtraElements]
+    [ClassProperty(Name = "用户优惠券", PageType = ViewPageType.List)]
+    public class UserCoupon : AggregateMongodbUserRoot<UserCoupon>, IAutoTable<UserCoupon> {
+
+        /// <summary>
+        /// 优惠券ID
+        /// </summary>
+        [Field(ControlsType = ControlsType.Hidden)]
+        public ObjectId CouponId { get; set; }
+
+        /// <summary>
+        /// 店铺Id，根据店铺来设置优惠券
+        /// </summary>
+        [Field(ControlsType = ControlsType.Hidden)]
+        public long StoreId { get; set; }
+
+        /// <summary>
+        /// 优惠券名称
+        /// </summary>
+        [Display(Name = "优惠券名称")]
+        [HelpBlock("必须填写,名称在10个字符以内")]
+        [Field(ControlsType = ControlsType.TextBox, IsShowBaseSerach = true, IsShowAdvancedSerach = true,
+            Width = "150", ListShow = true, EditShow = false, SortOrder = 2)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 消费满多少可用, 0不限制（使用条件）
+        /// </summary>
+        [Display(Name = "消费满多少可用")]
+        [Field(ControlsType = ControlsType.Numberic, ListShow = true, EditShow = false, SortOrder = 2)]
+        public decimal MinOrderPrice { get; set; } = 0;
+
+        /// <summary>
+        /// 优惠方式
+        /// </summary>
+        [Display(Name = "优惠方式")]
+        [HelpBlock("优惠方式分为：立减、打折")]
+        [Field(ControlsType = ControlsType.RadioButton, IsShowBaseSerach = false, IsShowAdvancedSerach = false,
+    Width = "150", ListShow = true, EditShow = true, SortOrder = 2)]
+        public CouponType Type { get; set; }
+
+        /// <summary>
+        /// 优惠金额
+        ///包括立减多少元，和打折折扣（折扣1以内）
+        /// </summary>
+        [Display(Name = "优惠金额")]
+        [HelpBlock("包括立减多少元，和打折折扣（折扣1以内）")]
+        [Field(ControlsType = ControlsType.TextBox, IsShowBaseSerach = false, IsShowAdvancedSerach = false,
+    Width = "150", ListShow = true, EditShow = true, SortOrder = 2)]
+        public decimal Value { get; set; }
+
+        /// <summary>
+        /// 开始有效期
+        /// </summary>
+        [Display(Name = "开始有效期")]
+        [HelpBlock("优惠券可使用的开始有效期")]
+        [Field(ControlsType = ControlsType.DateTimeRang, IsShowBaseSerach = false, IsShowAdvancedSerach = true,
+    Width = "150", ListShow = true, EditShow = true, SortOrder = 2)]
+        public DateTime StartValidityTime { get; set; }
+
+        /// <summary>
+        /// 结束有效期
+        /// </summary>
+        [Display(Name = "结束有效期")]
+        [HelpBlock("优惠券可使用的到期时间")]
+        [Field(ControlsType = ControlsType.DateTimeRang, IsShowBaseSerach = false, IsShowAdvancedSerach = true,
+    Width = "150", ListShow = true, EditShow = true, SortOrder = 2)]
+        public DateTime EndValidityTime { get; set; }
+
+        /// <summary>
+        /// 优惠券状态
+        /// </summary>
+        [Display(Name = "状态")]
+        [HelpBlock("优惠券状态：正常、已使用、已过期")]
+        [Field(ControlsType = ControlsType.DataSelect, DataSource = "Alabo.App.Shop.Coupons.Domain.Enums.CouponStatus", IsShowBaseSerach = false, IsShowAdvancedSerach = false,
+       Width = "150", ListShow = true, SortOrder = 2)]
+        public CouponStatus CouponStatus { get; set; }
+
+        public List<TableAction> Actions() {
+            return new List<TableAction>() {
+            };
+        }
+
+        public PageResult<UserCoupon> PageTable(object query, AutoBaseModel autoModel) {
+            var model = new PagedList<UserCoupon>();
+            var couponList = Resolve<IUserCouponService>().GetPagedList(query);
+            return null;
+        }
+    }
+}
---
+++ src/04.industry/02-Alabo.Industry.Shop/Deliveries/Domain/Entities/DeliveryTemplate.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using Alabo.App.Core.Api.Domain.Service;
+using Alabo.App.Shop.Store.Domain.Enums;
+using Alabo.App.Shop.Store.Domain.Services;
+using Alabo.Domains.Entities;
+using Alabo.Domains.Enums;
+using Alabo.Exceptions;
+using Alabo.UI;
+using Alabo.UI.AutoTables;
+using Alabo.Web.Mvc.Attributes;
+
+namespace Alabo.App.Shop.Store.Domain.Entities {
+
+    /// <summary>
+    /// 店铺运费模板
+    /// </summary>
+    [Table("Store_DeliveryTemplate")]
+    public class DeliveryTemplate : AggregateMongodbUserRoot<DeliveryTemplate> {
+
+        #region 属性
+
+        public long StoreId { get; set; }
+
+        /// <summary>
+        ///     运费模板方式
+        /// </summary>
+        [Display(Name = "运费模板名称")]
+        [Required]
+        [Field(ControlsType = ControlsType.TextBox, IsShowBaseSerach = true, IsShowAdvancedSerach = true, Width = "180", ListShow = true,
+            Operator = Datas.Queries.Enums.Operator.Contains,
+            EditShow = false, SortOrder = 1)]
+        public string TemplateName { get; set; }
+
+        /// <summary>
+        ///     运费模板方式
+        /// </summary>
+        [Display(Name = "运费模板方式")]
+        public DeliveryTemplateType TemplateType { get; set; }
+
+        /// <summary>
+        ///     运费计算方式
+        /// </summary>
+        [Display(Name = "运费计算方式")]
+        public DeliveryFreightCalculateType CalculateType { get; set; }
+
+        /// <summary>
+        ///     发货时间
+        /// </summary>
+        [Display(Name = "发货时间")]
+        public DeliveryTimeType DeliveryTime { get; set; } = DeliveryTimeType.DeliveryTimeTypef;
+
+        public Guid ExpressId { get; set; }
+
+        /// <summary>
+        ///     ExpressConfig的配置ID
+        /// </summary>
+        public ExpressType ExpressType { get; set; }
+
+        /// <summary>
+        ///     默认首重
+        /// </summary>
+        public decimal StartStandard { get; set; } = 1;
+
+        /// <summary>
+        ///     默认首费：输入0.01-999.99（最多包含两位小数） 不能为空也不能为0
+        /// </summary>
+        [Display(Name = "默认首费")]
+        [Field(IsShowBaseSerach = true, IsShowAdvancedSerach = true, Width = "180", ListShow = true,
+           EditShow = false, SortOrder = 3)]
+        public decimal StartFee { get; set; } = 8;
+
+        /// <summary>
+        ///     默认续费标准：输入1-9999范围内的整数
+        /// </summary>
+        public decimal EndStandard { get; set; } = 1;
+
+        /// <summary>
+        ///     默认续费：输入0.01-999.99（最多包含两位小数） 不能为空也不能为0
+        /// </summary>
+        public decimal EndFee { get; set; } = 6;
+
+        /// <summary>
+        ///     运费模板费用
+        ///     区域模板费用
+        /// </summary>
+        public IList<RegionTemplateFee> TemplateFees { get; set; } = new List<RegionTemplateFee>();
+
+        public bool Enabled { get; set; }
+
+        #endregion 属性
+
+        public PageResult<DeliveryTemplate> PageTable(object query, AutoBaseModel autoModel) {
+            var model = new PagedList<DeliveryTemplate>();
+            if (autoModel.Filter == FilterType.Admin) {
+                model = Resolve<IDeliveryTemplateService>().GetPagedList(query);
+            } else if (autoModel.Filter == FilterType.User) {
+                var store = Resolve<IShopStoreService>().GetUserStore(autoModel.BasicUser.Id);
+                if (store == null) {
+                    throw new ValidException("您不是供应商");
+                }
+                model = Resolve<IDeliveryTemplateService>().GetPagedList(query, r => r.StoreId == store.Id);
+            } else {
+                throw new ValidException("方式不对");
+            }
+            model.Result = model.Result.Select(s => {
+                //ConvertToApiImageUrl
+                s.UserName = Resolve<IApiService>().ConvertToApiImageUrl(s.UserName);
+                return s;
+            }).ToList();
+            return null;
+        }
+    }
+
+    /// <summary>
+    ///     运费模板费用
+    ///     区域模板费用
+    /// </summary>
+    public class RegionTemplateFee {
+
+        /// <summary>
+        ///     区域ID可以是城市ID，也可以是区域ID，也可以是省份ID
+        /// </summary>
+        public IList<long> RegionList { get; set; } = new List<long>();
+
+        //public string RegionName { get; set; }
+
+        /// <summary>
+        ///     首费标准：输入1-9999范围内的整数
+        /// </summary>
+        public decimal StartStandard { get; set; } = 1;
+
+        /// <summary>
+        ///     首费：输入0.01-999.99（最多包含两位小数） 不能为空也不能为0
+        /// </summary>
+        public decimal StartFee { get; set; } = 10;
+
+        /// <summary>
+        ///     续费标准：输入1-9999范围内的整数
+        /// </summary>
+        public decimal EndStandard { get; set; } = 1;
+
+        /// <summary>
+        ///     续费：输入0.01-999.99（最多包含两位小数） 不能为空也不能为0
+        /// </summary>
+        public decimal EndFee { get; set; } = 8;
+    }
+}
