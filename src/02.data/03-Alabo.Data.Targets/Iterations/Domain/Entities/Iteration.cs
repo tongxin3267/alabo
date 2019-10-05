ce5595f5ad0b1b9e98d258035a9576af28101144
zkcloud <asd@qq.com>
2019-10-05 22:01:38 +0800

    fix:迭代

--- src/02.data/03-Alabo.Data.Targets/Iterations/Domain/Entities/Iteration.cs
+++ src/02.data/03-Alabo.Data.Targets/Iterations/Domain/Entities/Iteration.cs
@@ -1,5 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Alabo.Data.Targets.Iterations.Domain.Enums;
 using Alabo.Domains.Entities;
+using Alabo.Domains.Enums;
+using Alabo.Validations;
 using Alabo.Web.Mvc.Attributes;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -13,5 +18,46 @@ namespace Alabo.Data.Targets.Iterations.Domain.Entities
     [ClassProperty(Name = "迭代", Description = "目标迭代")]
     public class Iteration : AggregateMongodbUserRoot<Iteration>
     {
+        /// <summary>
+        /// 迭代名称
+        /// </summary>
+        [Display(Name = "迭代名称")]
+        [Required(ErrorMessage = ErrorMessage.NameNotAllowEmpty)]
+        [Field(ListShow = true, SortOrder = 1, ControlsType = ControlsType.TextBox)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 迭代阶段
+        /// </summary>
+        [Display(Name = "阶段")]
+        [Required(ErrorMessage = ErrorMessage.NameNotAllowEmpty)]
+        [Field(ListShow = true, SortOrder = 1, ControlsType = ControlsType.RadioButton, DataSourceType = typeof(IterationPhase))]
+        public IterationPhase Phase { get; set; }
+
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+
+        [Display(Name = "开始时间")]
+        [Required(ErrorMessage = ErrorMessage.NameNotAllowEmpty)]
+        [Field(ListShow = false, SortOrder = 5, ControlsType = ControlsType.DateTimePicker)]
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+
+        [Display(Name = "结束时间")]
+        [Required(ErrorMessage = ErrorMessage.NameNotAllowEmpty)]
+        [Field(ListShow = false, SortOrder = 6, ControlsType = ControlsType.DateTimePicker)]
+        public DateTime EndTime { get; set; }
+
+        /// <summary>
+        /// 迭代名称
+        /// </summary>
+        [Display(Name = "迭代目标")]
+        [Required(ErrorMessage = ErrorMessage.NameNotAllowEmpty)]
+        [Field(ListShow = false, SortOrder = 10, ControlsType = ControlsType.TextBox)]
+        public string Intro { get; set; }
     }
 }
---
+++ src/02.data/03-Alabo.Data.Targets/Iterations/Domain/Enums/IterationPhase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Alabo.Web.Mvc.Attributes;
+
+namespace Alabo.Data.Targets.Iterations.Domain.Enums
+{
+    /// <summary>
+    /// 迭代阶段
+    /// </summary>
+    public enum IterationPhase
+    {
+        /// <summary>
+        ///     未开始
+        /// </summary>
+        [LabelCssClass(BadgeColorCalss.Success)]
+        [Display(Name = "未开始")]
+        NotBegin = 1,
+
+        /// <summary>
+        ///     进行中
+        /// </summary>
+        [LabelCssClass(BadgeColorCalss.Metal)]
+        [Display(Name = "进行中")]
+        OnGoing = 2,
+
+        /// <summary>
+        ///     已完成
+        /// </summary>
+        [LabelCssClass(BadgeColorCalss.Danger)]
+        [Display(Name = "已完成")]
+        Over = 3
+    }
+}
